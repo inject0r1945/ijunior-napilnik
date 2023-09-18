36b5e257485cb288bc4db7b1c06f592de73f104f
inject0r <di-vasilev90@ya.ru>
2023-09-18 11:38:51 +0700

    Первая домашняя работа

---
+++ 01_Weapons/Weapons.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW1
+{
+    public interface IDamageable
+    {
+        public void TakeDamage(int damage);
+    }
+
+    public class Health : IDamageable
+    {
+        public int Value { get; private set; }
+
+        public void TakeDamage(int damage)
+        {
+            Value -= damage;
+        }
+    }
+
+    public class Player : IDamageable
+    {
+        private Health _health;
+
+        public Player(Health health)
+        {
+            _health = health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            _health.TakeDamage(damage);
+        }
+    }
+
+    public class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+
+        public Weapon(int damage, int bullets)
+        {
+            _damage = damage;
+            _bullets = bullets;
+        }
+
+        public void Fire(IDamageable damageable)
+        {
+            if (_bullets == 0)
+                return;
+
+            damageable.TakeDamage(_damage);
+            _bullets--;
+        }
+    }
+
+    public class Bot
+    {
+        private Weapon _weapon;
+
+        public Bot(Weapon weapon)
+        {
+            _weapon = weapon;
+        }
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.Fire(player);
+        }
+    }
+}
